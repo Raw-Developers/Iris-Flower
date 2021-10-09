add33f8fc11d100951abcf9a9bb6a9cc62424aa8
Aishwarya G <aishwarya.g@thoughtworks.com>
2021-10-09 12:34:37 +0530

    Fix the numpy package name

--- iris.py
+++ iris.py
@@ -1,7 +1,7 @@
+import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-import numpy as np
 
 iris_dataset = load_iris()
 
